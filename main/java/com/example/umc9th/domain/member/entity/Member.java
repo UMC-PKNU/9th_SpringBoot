46f7cd8148447f75118d1abd66647c95ef4cd812
KING <dbswjddn6@gmail.com>
2025-10-12 18:23:42 +0900

    Feat: 4주차 미션

---
+++ main/java/com/example/umc9th/domain/member/entity/Member.java
@@ -0,0 +1,94 @@
+package com.example.umc9th.domain.member.entity;
+
+import com.example.umc9th.domain.inquiry.Inquiry;
+import com.example.umc9th.domain.location.Location;
+import com.example.umc9th.domain.member.enums.Gender;
+import com.example.umc9th.domain.member.enums.MemberStatus;
+import com.example.umc9th.domain.member.enums.PreferredFood;
+import com.example.umc9th.domain.membermission.MemberMission;
+import com.example.umc9th.domain.term.UserTerm;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "member")
+public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false, length=50)
+    private String username;
+
+    @Column(nullable=false, length=50)
+    private String nickname;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Column(nullable=false, length=20)
+    private String birth;
+
+    @Column(nullable = false, length=50)
+    private String address;
+
+    @CreatedDate
+    @Column(nullable=false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable=false)
+    private LocalDateTime updatedAt;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private MemberStatus status;
+
+    @Column(nullable = false)
+    private LocalDateTime inactiveAt;
+
+    @Column(nullable = false)
+    private int point;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private PreferredFood preferredFood;
+
+    @Column(nullable = false, length=20)
+    private String phoneNumber;
+
+    @Column(nullable=false, length=100)
+    private String email;
+
+    @Column(nullable=false)
+    private int successMissionCnt = 0;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<MemberMission> memberMissions = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<UserTerm> userTerms = new ArrayList<>();
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "location_id")
+    private Location location;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<Inquiry> inquiries = new ArrayList<>();
+}
+
---
+++ main/java/com/example/umc9th/domain/review/ReviewStatus.java
@@ -0,0 +1,6 @@
+package com.example.umc9th.domain.review;
+
+public enum ReviewStatus {
+    DRAFT,
+    PUBLISHED
+}
