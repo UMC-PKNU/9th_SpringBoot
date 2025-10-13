48bd3529867e07e525813140f4f02687fc02c609
dgun <hdg20042@gmail.com>
2025-10-13 21:27:48 +0900

    4주차 미션 업로드

---
+++ 주드/4주차_주드/src/main/java/com/example/umc9th/domain/member/entity/Member.java
@@ -0,0 +1,77 @@
+package com.example.umc9th.domain.member.entity;
+import com.example.umc9th.domain.review.entity.Review;
+//import com.example.umc9th.domain.member.entity.mapping.MemberFood;
+import com.example.umc9th.domain.member.entity.mapping.MemberMission;
+import com.example.umc9th.domain.member.enums.Gender;
+//import com.example.umc9th.domain.store.enums.Address;
+//import com.example.umc9th.global.auth.enums.SocialType;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.domain.Auditable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Table(name= "member")
+@EntityListeners(AuditingEntityListener.class)
+
+public class Member{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nickname", length = 20, nullable = false)
+    private String nickname;
+
+    @Column(name = "gender", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Column(name = "point")
+    @Builder.Default
+    private Long point = 0L;
+
+    @Column(name = "last_login_at")
+    private LocalDateTime last_login_at;
+
+    @Column(name = "is_active", nullable = false)
+    @Builder.Default
+    private boolean is_active = true;
+
+    @Column(name = "inactive_date")
+    private LocalDateTime inactive_date;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime created_at;
+
+    @Column(name = "phone_number", length = 11, nullable = false)
+    private String phone_number;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "local", nullable = false)
+    private String local;
+
+    @OneToMany(mappedBy = "member")
+    private List<MemberMission> memberMissionList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member")
+    private List<Review> reviewList = new ArrayList<>();
+
+
+}
