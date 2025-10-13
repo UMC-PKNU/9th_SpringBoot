18aa8a511ee93aeba9f1b5a0464011d238e2360f
KING <dbswjddn6@gmail.com>
2025-10-13 12:52:48 +0900

    .

--- 우디/4주차_우디/main/java/com/example/umc9th/domain/store/StoreStatus.java
+++
@@ -1,6 +0,0 @@
-package com.example.umc9th.domain.store;
-
-public enum StoreStatus {
-    ACTIVE,
-    INACTIVE
-}
--- 우디/4주차_우디/main/java/com/example/umc9th/domain/term/Category.java
+++
@@ -1,9 +0,0 @@
-package com.example.umc9th.domain.term;
-
-public enum Category {
-    AGE,
-    SERVICE,
-    PRIVACY,
-    LOCATION,
-    MARKETING
-}
